a4e221f2e9c2e068b30a96bb0ea6c49bd8113a52
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-10-05 23:18:45 +0100

    removed files

--- src/mymodule1.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    name: 'Elias',
-    email: 'test@email.com'
-}
--- src/mymodule2.js
+++
@@ -1,8 +0,0 @@
-export const person = {
-    name: 'John',
-    age: 30
-}
-
-export function sayHello(){
-    return `Hello ${person.name}`;
-}
